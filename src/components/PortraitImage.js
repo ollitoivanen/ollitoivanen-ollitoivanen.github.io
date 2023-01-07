bb169d5b7e585733f52d876569773b8bd2363b0a
Olli Toivanen <olliteed@gmail.com>
2023-01-07 22:05:28 +0200

    Added about me-section

--- src/components/PortraitImage.js
+++ src/components/PortraitImage.js
@@ -8,22 +8,8 @@ const PortraitImage = () => {
   );
 };
 
-const view_container = {
-  backgroundColor: "white",
-  borderStyle: "solid",
-  borderColor: "black",
-  borderWidth: 3,
-  borderRadius: 10,
-  margin: 8,
-  marginTop: 16,
-  marginBottom: 16,
-  paddingTop: 16,
-  paddingBottom: 16,
-  paddingRight: 16,
-};
 
 const image_portrait = {
-  height: 320,
-};
-
+  height: 300
+}
 export default PortraitImage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import * as React from "react";
 
+
 import PortraitImage from "../components/PortraitImage";
 
-const pageStyles = {
-  color: "#232129",
-  padding: 48,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
+const view_container = {
+  padding: 48
+}
 
 const text_header = {
   fontFamily: "Arvo Regular",
@@ -21,29 +20,59 @@ const text_name = {
   paddingLeft: 64,
 };
 
+const text_about_me_header = {
+  fontFamily: "Arvo Regular",
+  fontSize: 22,
+  color: "gray",
+  paddingLeft: 64,
+  marginTop: 64
+};
+
 const text_skills = {
   fontFamily: "Arvo Regular",
   fontSize: 18,
-  wrap: true,
 };
 
+const text_about_me = {
+  fontFamily: "Courier New",
+  fontSize: 20,
+  paddingLeft: 80,
+}
+
+const text_link = {
+  fontFamily: "Courier New",
+  fontSize: 20,
+}
+
 const view_skills_container = {
-  paddingTop: 6,
-  paddingLeft: 96,
-  borderWidth: 4,
+  display: "inline-block",
+  padding: 6,
+  paddingLeft: 36,
+  paddingRight: 36,
+  marginTop: 24,
+  marginLeft: 96,
+  borderStyle: "solid",
+  borderWidth: 3,
   borderColor: "black",
 };
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <PortraitImage></PortraitImage>
+    <main style={view_container}>
+      <PortraitImage />
       <h1 style={text_header}>Building things to last</h1>
       <h2 style={text_name}>Olli Toivanen</h2>
       <div style={view_skills_container}>
-        <p style={text_skills}>Full Stack Developement</p>
-        <p style={text_skills}>iOS / Android apps</p>
-        <p style={text_skills}>Design</p>
+        <a href="https://github.com/ollitoivanen" style={text_skills}>Full Stack Developement →</a>
+        <br/>
+
+        <a href="https://github.com/ollitoivanen" style={text_skills}>iOS / Android apps →</a>
       </div>
+      <h2 style={text_about_me_header}>About Me</h2>
+      <p style={text_about_me}>Hello, I'm Olli, a long time software hobbyist and a second year software engineering student.
+       I've been building my own apps and websites since 2017,
+        all of which you can see on my <a style={text_link} href="https://github.com/ollitoivanen?tab=repositories">GitHub</a>
+      .</p>
+
     </main>
   );
 };

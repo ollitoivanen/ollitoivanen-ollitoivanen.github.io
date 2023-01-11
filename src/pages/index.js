af0ae73ca570f41c9ee421c32a29628470d076c5
Olli Toivanen <olliteed@gmail.com>
2023-01-11 19:50:48 +0200

    Add longer about me

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,33 +4,40 @@ import * as React from "react";
 import PortraitImage from "../components/PortraitImage";
 
 const skills = ["React Native", "React", "SwifUI", "Node.js", "Databases"]
+const aboutMeText_1 = "Hello, I'm Olli, a long time software hobbyist and a second year software engineering student. I've been building my own apps and websites since 2017, all of which you can see on my " 
+const aboutMeText_2 = ". My journey started when I discoverd a problem as a footballer that I wanted to solve. Playing competetively meant that I wanted to train on my own time. However, there wasn't a way to know if there was any space on the field to train. So I started building an app to solve this, first with Java, then with React Native. With no background to software engineering the learning curve felt impossibly deep. But with patience and lots of googling, I learned everything needed to build an app with authentication, a database and user interaction."
+const aboutMeText_3 = "In the following years football became more of an hobby and I started taking software engineering even more seriously. I kept solving small inconveniences in my daily life with software, discovering new technologies and methodologies.  In 2020 I started my studies at University of Turku, studying Informations Technology. Here I have met many like-minded friends to study and improve with.  Now my days consist of coding, studying and running. I also have a great interest in hiking, cycling and photography."
 const IndexPage = () => {
   return (
     <main style={view_container}>
       <PortraitImage />
-      <h1 style={text_header}>Building things to last</h1>
-      <h2 style={text_name}>Olli Toivanen</h2>
-      <h2 style={text_name}><a style={text_personal_links} href={"https://www.linkedin.com/in/ollitoivanen/"}>LinkedIn</a></h2>
-      <h2 style={text_name}><a style={text_personal_links} href={"https://github.com/ollitoivanen"}>GitHub</a></h2>
-      <div style={view_skills_container}>
-        {skills.map(skill=>(
-        <p style={text_skills}>{skill}</p>
-        ))}      
+        <h1 style={text_header}>Building things to last</h1>
+        <h2 style={text_name}>Olli Toivanen</h2>
+        <h2 style={text_name}>
+          <a style={text_personal_links} href={"https://www.linkedin.com/in/ollitoivanen/"}>LinkedIn</a>
+        </h2>
+        <h2 style={text_name}>
+          <a style={text_personal_links} href={"https://github.com/ollitoivanen"}>GitHub</a>
+        </h2>
+        <div style={view_skills_container}>
+          {skills.map(skill=>(
+          <p style={text_skills}>{skill}</p>
+          ))}      
       </div>
-      
-      <h2 style={text_about_me_header}>About Me</h2>
-      <p style={text_about_me}>Hello, I'm Olli, a long time software hobbyist and a second year software engineering student.
-       I've been building my own apps and websites since 2017,
-        all of which you can see on my <a style={text_link} href="https://github.com/ollitoivanen">GitHub</a>
-      .</p>
-
+        <h2 style={text_about_me_header}>About Me</h2>
+          <p style={text_about_me}>
+            {aboutMeText_1}
+          <a style={text_link} href="https://github.com/ollitoivanen">GitHub</a>
+            {aboutMeText_2}
+          </p>
+          <p style={text_about_me}>{aboutMeText_3}</p>
     </main>
   );
 };
 
 
 const view_container = {
-  padding: 48
+  padding: 48,
 }
 
 const text_header = {
@@ -48,7 +55,7 @@ const text_about_me_header = {
   fontFamily: "Arvo Regular",
   fontSize: 22,
   color: "gray",
-  marginTop: 64
+  marginTop: 32
 };
 
 const text_skills = {
